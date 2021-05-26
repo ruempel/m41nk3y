ff2e3b9dafe0731f16d6079284d17af6e73d88e3
Andreas Rümpel <ruempel@gmail.com>
2021-05-26 16:10:42 +0200

    Update workbox dependency

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js");
 
 if (workbox) {
     // register third-party resources
